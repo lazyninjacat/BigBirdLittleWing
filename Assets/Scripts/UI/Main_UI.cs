d88991c1ca5b80beeba3ae2332e68a4fac96d5c8
Matt Riddett <35952057+lazyninjacat@users.noreply.github.com>
2020-06-28 12:47:23 -0700

    init commit

---
+++ Assets/Scripts/UI/Main_UI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_UI : MonoBehaviour
+{
+
+    [SerializeField] GameObject PauseMenu;
+
+    // Start is called before the first frame update
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (PauseMenu.activeSelf)
+            {
+                PauseMenu.SetActive(false);
+            }
+            else
+            {
+                PauseMenu.SetActive(true);
+            }
+        }
+
+        if (PauseMenu.activeSelf && Input.GetKeyDown("q key"))
+        {
+            SceneManager.LoadScene("StartMenuScene");
+        }
+    }
+}
