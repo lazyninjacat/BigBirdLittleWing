d560f115656c2aedbacf4f834daebc15ad637b15
carcat <carcatpsl@gmail.com>
2020-06-29 22:03:28 -0400

    Game ProjectFolder Reorganization

---
+++ Assets/Programmers/Scripts/Movement/PlayerManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] PlayerController[] players;
+    [SerializeField] int currentPlayerIndex = 0;
+    [SerializeField] KeyCode switchPlayers = KeyCode.Space;
+
+    [SerializeField] GameObject BigBirdCam;
+    [SerializeField] GameObject LittleWingCam;
+
+    private Camera mainCamera;
+    private PlayerController currentSub;
+
+    private void SwapPlayer()
+    {
+        //stop rb of previous player
+        currentSub.rb.velocity = Vector3.zero;
+
+        //just move through array normally
+        currentPlayerIndex = (currentPlayerIndex + 1 + players.Length) % players.Length;
+        currentSub = players[currentPlayerIndex];
+        //CameraSetup();
+        SwapActiveCamera();
+    }
+
+    private void SwapActiveCamera()
+    {
+        if (BigBirdCam.activeSelf)
+        {
+            BigBirdCam.SetActive(false);
+            LittleWingCam.SetActive(true);
+        }
+        else
+        {
+            LittleWingCam.SetActive(false);
+            BigBirdCam.SetActive(true);
+        }
+    }
+
+
+    //public void CameraSetup()
+    //{
+    //    mainCamera.transform.parent = currentSub.cameraPosition;
+    //    mainCamera.transform.localPosition = Vector3.zero;
+    //    mainCamera.transform.rotation = currentSub.transform.rotation;
+    //}
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        currentSub = players[currentPlayerIndex];
+        //CameraSetup();
+        SwapActiveCamera();
+    }
+
+    private void Update()
+    {
+        //check for sub switch here
+        if(Input.GetKeyUp(switchPlayers))
+        {
+            SwapPlayer();
+        }
+        else
+        {
+            currentSub.RunUpdate();
+        }
+
+      
+    }
+
+    private void FixedUpdate()
+    {
+        currentSub.RunFixedUpdate();
+    }
+
+    private void LateUpdate()
+    {
+        currentSub.RunLateUpdate();
+    }
+}
---
+++ Assets/Programmers/Scripts/Puzzle/PuzzlePiece.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    [SerializeField] private string group = "groupName";
+    public string GetGroup()
+    {
+        return group;
+    }
+    [SerializeField] private bool isActive = false;
+    public bool GetIsActive()
+    {
+        return isActive;
+    }
+    [SerializeField] private LayerMask triggerLayer;
+
+    //events
+    public static event Action<string> activated;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(triggerLayer == (triggerLayer | (1 << collision.gameObject.layer)))
+        {
+            isActive = true;
+            activated?.Invoke(group);
+            if (particles != null)
+            {
+                SetParticles();
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (triggerLayer == (triggerLayer | (1 << collision.gameObject.layer)))
+        {
+            isActive = false;
+            activated?.Invoke(group);
+            if(particles != null)
+            {
+                SetParticles();
+            }
+        }
+    }
+
+    //bubbles
+    private ParticlePlatform particles;
+
+    private void SetParticles()
+    {
+        particles.SetParticles(!isActive);
+    }
+
+    private void Start()
+    {
+        particles = GetComponent<ParticlePlatform>();
+        SetParticles();
+    }
+
+}
