d6c0db1befa83d8312d2adfa862373f8090fad50
Matt Riddett <35952057+lazyninjacat@users.noreply.github.com>
2020-07-14 18:27:40 -0700

    Revert "Changes"
    This reverts commit bb2c542055c15abe7c0b2540395caa2de546663c.

--- Assets/Seeding.cs
+++ Assets/Seeding.cs
@@ -11,14 +11,12 @@ public class Seeding : MonoBehaviour
     public PlantType _plantType;
     public Vector3 _Bounds = new Vector3(2, 1, 2);
     static int _maxObj = 1000;
-    Camera _mainCam;
-    [SerializeField] GameObject[] _spawnPrefab;
+
+    [SerializeField] GameObject _spawnPrefab;
     GameObject[] _objIndex = new GameObject[_maxObj];
     public int _spawnCount;
-    PlayerManager _player;
-    RaycastHit hit;
-    private void OnDrawGizmosSelected()
-    {
+      private void OnDrawGizmosSelected()
+    {       
         Gizmos.color = new Color(0, 1, 0, 1);
         Gizmos.DrawCube(transform.position, new Vector3(_Bounds.x * 2, _Bounds.y * 2, _Bounds.z * 2));
     }
@@ -26,52 +24,37 @@ public class Seeding : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _player = FindObjectOfType<PlayerManager>();
         for (int i = 0; i < _spawnCount; i++)
         {
-            Quaternion _rot;
             Vector3 pos = new Vector3(Random.Range(transform.position.x - _Bounds.x, transform.position.x + _Bounds.x),
                                      Random.Range(transform.position.y - _Bounds.y, transform.position.y + _Bounds.y),
                                      Random.Range(transform.position.z - _Bounds.z, transform.position.z + _Bounds.z));
 
+            _objIndex[i] = (GameObject)Instantiate(_spawnPrefab, pos, Quaternion.identity);
+            _objIndex[i].transform.parent = this.transform;
+
             switch (_plantType)
             {
                 case PlantType.Algae_Red:
-                    Physics.Raycast(pos, -Vector3.up, out hit, Mathf.Infinity);
-                    _objIndex[i] = (GameObject)Instantiate(_spawnPrefab[Random.Range(0, _spawnPrefab.Length)], hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
-                    _objIndex[i].transform.parent = this.transform;
+                    _objIndex[i].GetComponent<growth>()._isColor = true;
                     _objIndex[i].layer = 12;
                     break;
                 case PlantType.Anemone:
-                    Physics.Raycast(pos, -Vector3.up, out hit, Mathf.Infinity);
-                    _objIndex[i] = (GameObject)Instantiate(_spawnPrefab[Random.Range(0, _spawnPrefab.Length)], hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
-                    _objIndex[i].transform.parent = this.transform;
                     _objIndex[i].layer = 10;
                     break;
                 case PlantType.SeaWeed:
-                    Physics.Raycast(pos, -Vector3.up, out hit, Mathf.Infinity);
-                    _objIndex[i] = (GameObject)Instantiate(_spawnPrefab[Random.Range(0, _spawnPrefab.Length)], hit.point, Quaternion.FromToRotation(Vector3.up, Vector3.zero));
-                    _objIndex[i].transform.parent = this.transform;
                     _objIndex[i].layer = 12;
+                    _objIndex[i].GetComponent<growth>()._isSeaweed = true;
                     break;
                 case PlantType.Fish:
-                    _objIndex[i] = (GameObject)Instantiate(_spawnPrefab[0], pos, Quaternion.identity);
-                    _objIndex[i].transform.parent = this.transform;
                     _objIndex[i].layer = 11;
                     break;
                 case PlantType.Coral:
-                    Physics.Raycast(pos, -Vector3.up, out hit, Mathf.Infinity);
-                    _objIndex[i] = (GameObject)Instantiate(_spawnPrefab[Random.Range(0, _spawnPrefab.Length)], hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
-                    _objIndex[i].transform.parent = this.transform;
                     _objIndex[i].layer = 12;
                     break;
                 default:
                     break;
             }
-            _objIndex[i].transform.localScale = transform.localScale / Random.Range(0.2f, 2);
-            _rot = _objIndex[i].transform.rotation;
-            _rot.y = Random.Range(0, 360);
-            transform.rotation = _rot;
         }
     }
 
@@ -101,6 +84,9 @@ public class Seeding : MonoBehaviour
                 break;
             default:
                 break;
-        }
+        }    
     }
+    ///Rules
+    ///if too close destroy one of the seedlings
+    ///
 }
