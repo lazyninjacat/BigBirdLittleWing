8b91d1e6bdd4dd36d845f653b86eefaa34dff716
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2020-07-15 16:57:37 -0700

    First 4 sections loading in and populated

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager L_Instance;
+
+    [SerializeField] public GameObject[] _levelLoaders;
+    PlayerManager _player;
+    bool _managers;
+    public bool _load01 = false;
+    public bool _load02 = false;
+    public bool _load03 = false;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        if (L_Instance == null)
+            L_Instance = this;
+        else if (L_Instance != this)
+            Destroy(L_Instance);
+        DontDestroyOnLoad(this.gameObject);
+        _player = FindObjectOfType<PlayerManager>();
+        if (!_managers)
+        {
+            SceneManager.LoadSceneAsync("ALLMANAGERS", LoadSceneMode.Additive);
+            _managers = true;
+        }
+    }
+
+    private void Update()
+    {
+     
+    }
+    public void LoadScene(string x)
+    {
+        SceneManager.LoadSceneAsync(x, LoadSceneMode.Additive);
+    }
+    public void UnloadScene(string x)
+    {
+        SceneManager.UnloadSceneAsync(x);
+    }
+}
---
+++ Assets/LoadSection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSection : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 29 || other.gameObject.layer == 30)
+        {
+            switch (this.gameObject.name)
+            {
+                case "Load_01":
+                    if (!LevelManager.L_Instance._load01)
+                    {
+                        LevelManager.L_Instance.LoadScene("Section1");
+                        LevelManager.L_Instance._load01 = true;
+                        LevelManager.L_Instance.UnloadScene("Section3");
+                        LevelManager.L_Instance._load03 = false;
+                    }
+                    break;
+                case "Load_02":
+                    if (!LevelManager.L_Instance._load02)
+                    {
+                        LevelManager.L_Instance.LoadScene("Section2");
+                        LevelManager.L_Instance._load02 = true;
+                    }
+                    break;
+                case "Load_03":
+                    if (!LevelManager.L_Instance._load03)
+                    {
+                        LevelManager.L_Instance.LoadScene("Section3");
+                        LevelManager.L_Instance._load03 = true;
+                        LevelManager.L_Instance.UnloadScene("Section1");
+                        LevelManager.L_Instance._load01 = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+     
+        }
+    }
+}
