c7d96c938b4cbbb028ee44e70c52719af468e972
Matt Riddett <35952057+lazyninjacat@users.noreply.github.com>
2020-08-03 22:13:53 -0700

    added a pause menu with controls guide

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject PauseMenuPanel;
+    private bool pauseMenuIsOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuIsOpen = false;
+        PauseMenuPanel.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7))
+        {
+            if (pauseMenuIsOpen)
+            {
+                PauseMenuPanel.SetActive(false);
+                pauseMenuIsOpen = false;
+            }
+            else
+            {
+                PauseMenuPanel.SetActive(true);
+                pauseMenuIsOpen = true;
+
+            }
+        }
+    }
+}
